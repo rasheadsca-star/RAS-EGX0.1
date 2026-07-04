be31fc990d5796833db83d2f3300c48abac246c3
rasheadsca-star <rasheadsca@gmail.com>
2026-07-04 11:43:04 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* EGX Pro Hub V9.2.3.2.1 Safe PWA Service Worker */
-const CACHE_NAME = "egx-pro-hub-shell-v9-2";
+/* EGX Pro Hub V9.2.1.3.2.1 Safe PWA Service Worker */
+const CACHE_NAME = "egx-pro-hub-shell-v9-2-1";
 const SHELL_FILES = [
   "./",
   "./index.html",
